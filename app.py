171e3feb175b72be3c4a7597feca73214f82f124
KunalLokhande99 <166371519+KunalLokhande99@users.noreply.github.com>
2024-04-08 16:55:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def addition(a,b):
 def subtraction(a,b):
     return a-b
 
-### changes from developer a
+### changes from developer a branch
 def multiplication(a,b):
     return a*b
 def division(a,b):
